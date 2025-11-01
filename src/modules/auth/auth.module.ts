00330bc6ba21998fad803e399695504395f7a470
go-tiger <dev.gotiger@gmail.com>
2025-11-02 07:25:21 +0900

    fix: Router Module Path 변경

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MicrosoftModule } from './microsoft/microsoft.module';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [MicrosoftModule],
+  imports: [
+    RouterModule.register([
+      {
+        path: 'auth',
+        children: [
+          {
+            path: '',
+            module: MicrosoftModule,
+          },
+        ],
+      },
+    ]),
+    MicrosoftModule,
+  ],
 })
 export class AuthModule {}
