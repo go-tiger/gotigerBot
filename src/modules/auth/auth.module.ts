dfd388ca1373aa55b1cfc858df5dd137bd8bd050
go-tiger <dev.gotiger@gmail.com>
2025-11-02 07:17:45 +0900

    chore: microsoft 리소스(임시)

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MicrosoftModule } from './microsoft/microsoft.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [MicrosoftModule],
 })
 export class AuthModule {}
