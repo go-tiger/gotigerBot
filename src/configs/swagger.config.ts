f6144b33c6142961976a904707341647503a3a2e
go-tiger <dev.gotiger@gmail.com>
2025-11-02 05:16:14 +0900

    feat: swagger 세팅

---
+++ src/configs/swagger.config.ts
@@ -0,0 +1,8 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder().setTitle('고랑봇 API').setDescription('API Docs').setVersion('1.0.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { setupSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
+  setupSwagger(app);
   await app.listen(port);
+  console.log('🚀 Server ON: ', await app.getUrl());
+  console.log(`🚀 Swagger: ${await app.getUrl()}/docs`);
 }
 bootstrap();
