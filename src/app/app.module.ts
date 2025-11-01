465fa9c953a54e5ca632b9e5c2e5f5cb48c12a48
go-tiger <dev.gotiger@gmail.com>
2025-11-02 07:16:10 +0900

    chore: auth 리소스(임시)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from 'src/configs/typeorm.config';
+import { AuthModule } from 'src/modules/auth/auth.module';
 import { DiscordModule } from 'src/modules/discord/discord.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync(typeormConfig()), DiscordModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeormConfig()),
+    AuthModule,
+    DiscordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
