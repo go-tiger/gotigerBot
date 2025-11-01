62614b610f5697198cb5e191ffa8e01298766240
go-tiger <dev.gotiger@gmail.com>
2025-11-02 05:21:03 +0900

    feat: typeorm 세팅 - 트랜젝션 설정 - 네이밍 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,17 +1,23 @@
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 export const typeormConfig = (): TypeOrmModuleAsyncOptions => ({
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
-    type: 'postgres',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USER'),
-    password: configService.get<string>('DB_PASS'),
-    database: configService.get<string>('DB_NAME'),
-    autoLoadEntities: true,
-    synchronize: configService.get<string>('NODE_ENV') !== 'production',
-    logging: configService.get<string>('NODE_ENV') === 'development',
-  }),
+  useFactory: async (configService: ConfigService) => {
+    initializeTransactionalContext();
+    return {
+      type: 'postgres',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASS'),
+      database: configService.get<string>('DB_NAME'),
+      autoLoadEntities: true,
+      synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      logging: configService.get<string>('NODE_ENV') === 'development',
+      namingStrategy: new SnakeNamingStrategy(),
+    };
+  },
 });
