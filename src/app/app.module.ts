2d1e395e73ba195b134a37d4b878a166b1cc98ab
go-tiger <dev.gotiger@gmail.com>
2025-11-02 20:55:09 +0900

    feat: @nestjs/serve-static 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from 'src/configs/typeorm.config';
 import { AuthModule } from 'src/modules/auth/auth.module';
 import { DiscordModule } from 'src/modules/discord/discord.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeormConfig()),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'public'),
+    }),
     AuthModule,
     DiscordModule,
   ],
